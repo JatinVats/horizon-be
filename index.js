a7214c05431e03ede62cd89fad34ac64e31ef6c5
Jatin Vats <104900558+JatinVats@users.noreply.github.com>
2023-06-16 15:02:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://main--merry-phoenix-5a4edc.netlify.app",
     credentials: true,
   },
 });
